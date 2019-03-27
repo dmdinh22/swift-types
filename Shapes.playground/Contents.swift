e1f14645275d4ca45ba8aad3b3875cf62fbda5ac
David Dinh <dmdinh22@gmail.com>
2019-03-27 00:47:06 -0700

    create playground

---
+++ Shapes.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum ColorName: String {
+    case black = "black"
+    case silver = "silver"
+    case gray = "gray"
+    case white = "white"
+    case maroon = "maroon"
+    case red = "red"
+    // etc.
+}
