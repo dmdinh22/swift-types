c56b8131fe29b4c095089271532ecf60c4257cdb
David Dinh <dmdinh22@gmail.com>
2019-03-27 21:01:26 -0700

    caseIterable

--- Shapes.playground/Contents.swift
+++ Shapes.playground/Contents.swift
@@ -2,10 +2,13 @@ import Foundation
 
 // enums makes string the same as
 // name of case when you don't specify
-enum ColorName: String {
+enum ColorName: String, CaseIterable {
     case black, silver, gray, white, maroon, red, purple, fuchsia, green,
     lime, olive, yellow, navy, blue, teal, aqua
 }
 
 let fill = ColorName.gray
-
+ 
+for color in ColorName.allCases {
+    print("I love the color \(color).")
+}
