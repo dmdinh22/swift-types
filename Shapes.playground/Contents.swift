5fbf130248d20f247aa8d2590a62e332398e43cf
David Dinh <dmdinh22@gmail.com>
2019-03-27 21:15:08 -0700

    associated values with enums

--- Shapes.playground/Contents.swift
+++ Shapes.playground/Contents.swift
@@ -2,6 +2,7 @@ import Foundation
 
 // enums makes string the same as
 // name of case when you don't specify
+// CaseIterable makes the enum iterable in order of definition
 enum ColorName: String, CaseIterable {
     case black, silver, gray, white, maroon, red, purple, fuchsia, green,
     lime, olive, yellow, navy, blue, teal, aqua
@@ -12,3 +13,33 @@ let fill = ColorName.gray
 for color in ColorName.allCases {
     print("I love the color \(color).")
 }
+
+// Associated Values - gives two cases
+// ie. CSSColor can be named - associated with ColorName
+// OR rgb - associated with rgb (2nd case)
+enum CSSColor {
+    case named(name: ColorName)
+    case rgb(red: UInt8, green: UInt8, blue: UInt8)
+}
+
+// enum extension to get string representation
+// adopt standard Swift lib protocols to inter-operate
+extension CSSColor: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .named(let colorName):
+            return colorName.rawValue
+        case .rgb(let red, let green, let blue):
+            return String(format: "#%02X%02X%02X", red, green, blue)
+        }
+    }
+}
+
+let color1 = CSSColor.named(name: .red)
+let color2 = CSSColor.rgb(red: 0xAA, green: 0xAA, blue: 0xAA)
+
+print("color 1 = \(color1), color2 = \(color2)")
+
+// Note: The extension style is nice because it makes what you define fully explicit, in order to conform to a given protocol.
+// In the case of CustomStringConvertible, you’re required to implement a getter for description.
+
